1c89db0548d21bcba4d60b096eedef176e916388
LhaisCosta99 <lhaisferreirada@gmail.com>
2023-02-06 19:54:36 -0300

    Infra da projeto iniciado(criação de tabela BD)

---
+++ src/infraestrutura/conexão.js
@@ -0,0 +1,12 @@
+require('dotenv').config()
+
+const mysql = require("mysql2");
+
+const conexao = mysql.createConnection({
+    host: process.env.HOSTNAME_BD,
+    port: 3000,
+    user: process.env.LOGIN_BD,
+    password: process.env.SENHA_BD,
+    database: process.env.BANCO_DE_DADOS
+})
+module.exports = conexao;
